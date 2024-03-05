68f9532dd65e7c19b360c168ab4e288c34e26a61
Prasun <maitypro402@gmail.com>
2024-03-05 18:49:57 +0000

    Adding variables and datatypes in 01_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 143243
+let accountEmail = "maitypro402" 
+var accountPassword = "12345" // we do not use var cuz of scope({}) problem in JS
+accountCity = "Jaipur"  //prefer not to use this bad practice
+let accountState
+// accountId = 123432 // bad practice
+accountEmail = "abcd"
+accountPassword = 44333
+accountCity = "abbbb"
+
+
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treats all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser console
+
+console.log(
+    3
+    +
+    3
+)   // bad practice code readibility should be high
+
+console.log("Prasun")
+
+let name = "Prasun"
+let age = 21
+let isLoggedIn = false
+let state
+
+// number => 2**53
+// bigint
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
