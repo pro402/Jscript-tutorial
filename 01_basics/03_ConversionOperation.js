d3df5132cd53c333788a4d23673c6a20b45553ea
Prasun <maitypro402@gmail.com>
2024-03-06 02:57:53 +0000

    Conversion Of Data Types

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,60 @@
+let s = "33"
+
+console.log(typeof s)
+console.log(typeof(s))
+
+let veN = Number(s)
+console.log(typeof veN)
+console.log(veN)
+
+let se = "33abc"
+
+console.log(typeof se)
+console.log(typeof(se))
+
+let vNr = Number(se)
+console.log(typeof vNr)
+console.log(vNr)
+
+let sc = null
+
+console.log(typeof sc)
+console.log(typeof(sc))
+
+let vNu = Number(sc)
+console.log(typeof vNu)
+console.log(vNu)
+
+let sco = true
+
+console.log(typeof sco)
+console.log(typeof(sco))
+
+let valueNumber = Number(sco)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+let score = "Prasun"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let vN = Number(score)
+console.log(typeof vN)
+console.log(vN)
+
+/*
+    Number()
+    "33" => number (33)
+    "33abc" => number but value after conversion is NaN(not a number)
+    null => number (0)
+    true => number (1)
+    "Prasun" => number (NaN)
+
+    Like Number() we also have 
+    String()
+    Boolean()
+    and many more .
+*/
+
+
