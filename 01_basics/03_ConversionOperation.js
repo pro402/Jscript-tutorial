e03f2827449808fd951aa9898c13728824940639
Prasun <maitypro402@gmail.com>
2024-03-06 03:16:30 +0000

    adding some lines in 03_ConversionOperation.js

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,60 +1,75 @@
-let s = "33"
+// let s = "33"
 
-console.log(typeof s)
-console.log(typeof(s))
+// console.log(typeof s)
+// console.log(typeof(s))
 
-let veN = Number(s)
-console.log(typeof veN)
-console.log(veN)
+// let veN = Number(s)
+// console.log(typeof veN)
+// console.log(veN)
 
-let se = "33abc"
+// let se = "33abc"
 
-console.log(typeof se)
-console.log(typeof(se))
+// console.log(typeof se)
+// console.log(typeof(se))
 
-let vNr = Number(se)
-console.log(typeof vNr)
-console.log(vNr)
+// let vNr = Number(se)
+// console.log(typeof vNr)
+// console.log(vNr)
 
-let sc = null
+// let sc = null
 
-console.log(typeof sc)
-console.log(typeof(sc))
+// console.log(typeof sc)
+// console.log(typeof(sc))
 
-let vNu = Number(sc)
-console.log(typeof vNu)
-console.log(vNu)
+// let vNu = Number(sc)
+// console.log(typeof vNu)
+// console.log(vNu)
 
-let sco = true
+// let sco = true
 
-console.log(typeof sco)
-console.log(typeof(sco))
+// console.log(typeof sco)
+// console.log(typeof(sco))
 
-let valueNumber = Number(sco)
-console.log(typeof valueNumber)
-console.log(valueNumber)
+// let valueNumber = Number(sco)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
 
-let score = "Prasun"
+// let score = "Prasun"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let vN = Number(score)
-console.log(typeof vN)
-console.log(vN)
+// let vN = Number(score)
+// console.log(typeof vN)
+// console.log(vN)
 
-/*
-    Number()
-    "33" => number (33)
-    "33abc" => number but value after conversion is NaN(not a number)
-    null => number (0)
-    true => number (1)
-    "Prasun" => number (NaN)
+// /*
+//     Number()
+//     "33" => number (33)
+//     "33abc" => number but value after conversion is NaN(not a number)
+//     null => number (0)
+//     true => number (1)
+//     "Prasun" => number (NaN)
 
-    Like Number() we also have 
-    String()
-    Boolean()
-    and many more .
-*/
+//     Like Number() we also have 
+//     String()
+//     Boolean()
+//     and many more .
+// */
 
+
+// **********************Operations*************************
+
+let val = 10
+let nagVal = - val;
+console.log(nagVal)
+
+let s1 = "Hello"
+let s2 = " Prasun"
+console.log(s1+s2);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); // => string
+console.log(1+2+"2"); // => first (1+2)+"2" => 32(string)
 
