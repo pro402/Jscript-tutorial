ba47cc84e4f4c30a319a749911af8fb3fa22dc56
Prasun <maitypro402@gmail.com>
2024-03-06 14:14:39 +0000

    modifying 04_Comparisions.js

--- 01_basics/04_Comparision.js
+++ 01_basics/04_Comparision.js
@@ -9,5 +9,20 @@ console.log("2" > 3);        //bad practice
 console.log("02" > 3);       //bad practice
 console.log("1"  ==  true) ; //bad practice
 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);      // why true
+
+// the reason is that an equality check == and comparisions >< >= 
+// <= works differently
+// Comparisions convert null to a number , treating it as 0.
+// that's why (0) null >= 0 is true and (1) null > 0 is false.
+
+// === is strictly check (it checks value along with data type)
+// good practice
+
+console.log("2" === 1);        
+console.log("1"  ===  true) ; 
+console.log(null == 0);
 
 
