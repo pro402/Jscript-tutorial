75fe9033e9e6aee8ad65cb297606eb78c45e8e38
Prasun <maitypro402@gmail.com>
2024-03-06 13:58:47 +0000

    creating a new file named 04_Comparision.js

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,13 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 == 1);
+console.log(2 < 1);
+console.log(2 != 1);
+
+console.log("2" > 1);        //bad practice 
+console.log("2" > 3);        //bad practice
+console.log("02" > 3);       //bad practice
+console.log("1"  ==  true) ; //bad practice
+
+
+
