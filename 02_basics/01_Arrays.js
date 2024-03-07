9f46a69f7486779fe98b363dfe60af7e85524b63
Prasun <maitypro402@gmail.com>
2024-03-07 15:56:40 +0000

    Adding Arrays file in the repo

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,41 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr," ",myHeors," ",myArr2);
+
+// console.log(myArr);
+// Array Method 
+
+// const j = myArr.toString();
+// console.log(j);
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.push(7)
+// myArr.push(8)
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newAr = myArr.join();
+
+// console.log(myArr);
+// console.log(newAr);
+
+// slice 
+console.log("A",myArr);
+const m1 = myArr.slice(1,3)
+console.log(m1);
+console.log("B",myArr);
+// splice
+const m2 = myArr.splice(1,3)
+console.log(m2);
+console.log("c",myArr);
+
+// console.log(myArr);
+// console.log(typeof myArr);
