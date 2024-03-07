782c66f5ecd657ef52e95691367e03c9bf5b53aa
Prasun <maitypro402@gmail.com>
2024-03-07 18:29:58 +0000

    adding some lines of code

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -9,8 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
-
+const all_new_heros = [...marvel_heros, ...dc_heros]  //scatter operation ...marvel_heros
 // console.log(all_new_heros);
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
