7ecfb3fc3517e74a2d8ffc8c8131be93aeeff54a
Prasun <maitypro402@gmail.com>
2024-03-07 08:18:14 +0000

    creating a dateTime.js file

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,13 @@
+// Dates
+
+let date = new Date()
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+// console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+console.log(date.toLocaleTimeString());
+console.log(date.toTimeString());
+console.log(date.toUTCString());
